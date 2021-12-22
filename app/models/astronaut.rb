b1bea4386205e79ae1882526a3a151539dc28e44
Tim Roeder <timothyjroeder@gmail.com>
2021-12-22 11:52:44 -0700

    Feat: add ::average_age, #mission_order_alpha, #total_time

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,4 +3,20 @@ class Astronaut < ApplicationRecord
 
   has_many :astronaut_missions
   has_many :missions, through: :astronaut_missions
+
+  # Class Methods
+  
+  def self.average_age
+    average(:age)
+  end
+
+  # Instance Methods
+  
+  def mission_order_alpha
+    missions.order(title: :asc)
+  end
+
+  def total_time
+    missions.sum(:time_in_space)
+  end
 end
