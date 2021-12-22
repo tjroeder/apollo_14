bde05e20a5a0de5f8c95b777624d24178d1d6b92
Tim Roeder <timothyjroeder@gmail.com>
2021-12-22 10:39:19 -0700

    Feat: add relationships and tests

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -8,7 +8,7 @@ describe Astronaut, type: :model do
   end
 
   describe 'Relationships' do
-    it { should have_many :astronaut_missions}
-    it { should have_many :missions}
+    it { should have_many :astronaut_missions }
+    it { should have_many(:missions).through(:astronaut_missions) }
   end
 end
